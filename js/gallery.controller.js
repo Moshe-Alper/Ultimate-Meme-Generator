caaa94cb487fcbf4142bb25be605ddd5c5c15437
Moshik Alper <moshikalper@gmail.com>
2024-08-03 13:49:43 +0300

    Create gallery.controller with renderGallery() func to rnder two img

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,1 +1,15 @@
 'use strict'
+
+renderGallery()
+
+function renderGallery() {
+    const elImgGallery = document.querySelector('.gallery-section')
+    console.log('elImgGallery:', elImgGallery)
+
+    elImgGallery.innerHTML = `
+                <article class="meme-list-item"><img src="/meme-imgs/1.jpg" alt="meme-image"></article>
+                <article class="meme-list-item"><img src="/meme-imgs/2.jpg" alt="meme-image"></article>
+    `
+
+    // elImgGallery.innerHTML = strHTMLs.join('')
+}
